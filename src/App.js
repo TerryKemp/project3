8a90963a85311634bcb0a97ea6c1f32cb91318af
Terry Kemp <Terry.kemp14@gmail.com>
2018-07-10 15:55:08 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">HEIR POLLUTION</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src='http://www.coresponsibility.com/wp-content/uploads/2016/06/air-pollution_cropped.jpg' className='App-logo' alt='logo' />
+          <h1 className='App-title'>HEIR POLLUTION</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className='App-intro'>
+                        WELCOME TO HEIR POLLUTION.WHERE YOU HAVE INHERITED THE PREVIOUS GENERATIONS PROBLEMS < /p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
