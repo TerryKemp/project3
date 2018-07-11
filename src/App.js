ec612c642ae4374ef83407782a86aa0ae9ca102d
Marsel Gray <marselgray@gmail.com>
2018-07-11 10:23:08 -0400

    basics

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react';
+import Titles from './components/Titles.js';
+import Form from './components/Form.js';
+import Weather from './components/Weather.js';
 
-class App extends Component {
-  render () {
-    return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src='http://www.coresponsibility.com/wp-content/uploads/2016/06/air-pollution_cropped.jpg' className='App-logo' alt='logo' />
-          <h1 className='App-title'>HEIR POLLUTION</h1>
-        </header>
-        <p className='App-intro'>
-                        WELCOME TO HEIR POLLUTION.WHERE YOU HAVE INHERITED THE PREVIOUS GENERATIONS PROBLEMS < /p>
-      </div>
-    )
-  }
+
+const API_KEY = 'cad2d6dddccc9804f43e7c3af9e56f52';
+
+
+class App extends React.Component {
+    render(){
+        return (
+            <div>
+                <Titles />
+                <Form />
+                <Weather />
+            </div>
+        )
+    }
 }
 
 export default App
+
+
+
+
+
+// class App extends Component {
+//   render () {
+//     return (
+//       <div className='App'>
+//         <header className='App-header'>
+//           <img src='http://www.coresponsibility.com/wp-content/uploads/2016/06/air-pollution_cropped.jpg' className='App-logo' alt='logo' />
+//           <h1 className='App-title'>HEIR POLLUTION</h1>
+//         </header>
+//         <p className='App-intro'>
+//                         WELCOME TO HEIR POLLUTION.WHERE YOU HAVE INHERITED THE PROBLEMS OF PREVIOUS GENERATIONS < /p>
+//       </div>
+//     )
+//   }
+// }
+
+// export default App
---
+++ src/components/Titles.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Titles extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>Heir Pollution</h1>
+                <p>You have inherited the problems from past misconduct</p>
+
+            </div>
+        )
+    }
+}
+
+export default Titles
