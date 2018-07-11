fbd97c78c6d45f7fd19a416902c87c92f82bbe75
Terry Kemp <Terry.kemp14@gmail.com>
2018-07-11 13:19:03 -0400

    Added Signup and Registration Form

--- src/App.js
+++ src/App.js
@@ -1,20 +1,57 @@
-import React, { Component } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, {Component} from 'react';
+import './App.css';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import {Motion, spring} from 'react-motion';
+import NavigationPanel from './components/NavigationPanel';
+import Modal from './components/Modal';
 
 class App extends Component {
-  render () {
-    return (
-      <div className='App'>
-        <header className='App-header'>
-          <img src='http://www.coresponsibility.com/wp-content/uploads/2016/06/air-pollution_cropped.jpg' className='App-logo' alt='logo' />
-          <h1 className='App-title'>HEIR POLLUTION</h1>
-        </header>
-        <p className='App-intro'>
-                        WELCOME TO HEIR POLLUTION.WHERE YOU HAVE INHERITED THE PREVIOUS GENERATIONS PROBLEMS < /p>
-      </div>
-    )
-  }
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			mounted: false
+		};
+	}
+
+	componentDidMount() {
+		this.setState({ mounted: true });
+	}
+	
+	handleSubmit = (e) => {
+		this.setState({ mounted: false });
+		e.preventDefault();
+	}
+
+	render() {
+		const {mounted} = this.state;
+
+		let child;
+		let test = 12;
+
+		if(mounted) {
+			child = (
+				<div className="App_test">
+					<NavigationPanel></NavigationPanel>
+					<Modal onSubmit={this.handleSubmit}/>
+				</div>
+			);
+		}
+		
+		return(
+			
+			<div className="App">
+				<ReactCSSTransitionGroup 
+					transitionName="example"
+					transitionEnterTimeout={500}
+					transitionLeaveTimeout={300}>
+						{child}
+				</ReactCSSTransitionGroup>
+		
+			</div>
+		);
+	}
 }
 
-export default App
+export default App;
+
