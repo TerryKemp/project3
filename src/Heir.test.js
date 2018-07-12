70c574598e37e159c7b8c50825d644d7b45359e6
Marsel Gray <marselgray@gmail.com>
2018-07-12 09:50:55 -0400

    renamed app to heir

--- src/Heir.test.js
+++ src/Heir.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Heir from './Heir';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Heir />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
